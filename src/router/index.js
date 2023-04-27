99af83c2a4a9456d9ea78386bd23db77d58707c9
Marilka888 <marilka888@mail.ru>
2023-04-28 02:29:00 +0300

    JEW-010 add routes and img

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,65 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import LoginVuew from '../views/LoginVuew.vue';
+import Vue from 'vue';
+import Router from 'vue-router';
 
-const routes = [
-  { path: '/ups', name: 'home', component: LoginVuew },
-  // { path: '/about', component: About },
-];
+Vue.use(Router);
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+export default new Router({
+  linkExactActiveClass: 'active',
+  routes: [
+    {
+      path: '*',
+      redirect: '/',
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login.vue'),
+    },
+    {
+      path: '/admin',
+      // name: 'Dashboard',
+      component: () => import('@/views/Dashboard.vue'),
+      children: [
+        {
+          path: 'productsmanage',
+          name: 'ProductsManage',
+          component: () => import('@/views/Admin/ProductsManage.vue'),
+          meta: { requiresAuth: true },
+        },
+        {
+          path: 'ordersmanage',
+          name: 'OrdersManage',
+          component: () => import('@/views/Admin/OrdersManage.vue'),
+          meta: { requiresAuth: true },
+        },
+      ],
+    },
+    {
+      path: '/',
+      // name: 'Layout',
+      component: () => import('@/views/Layout.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'Home',
+          component: () => import('@/views/Front/Home.vue'),
+        },
+        {
+          path: 'productslist',
+          name: 'ProductsList',
+          component: () => import('@/views/Front/ProductsList.vue'),
+        },
+        {
+          path: 'productslist/:productId',
+          name: 'Product',
+          component: () => import('@/views/Front/Product.vue'),
+        },
+        {
+          path: 'createorder',
+          name: 'CreateOrder',
+          component: () => import('@/views/Front/CreateOrder.vue'),
+        },
+      ],
+    },
+  ],
 });
-
-export default router;
-
-// 5. Create and mount the root instance.
-// const app = Vue.createApp({})
-// app.use(router)
-// app.mount('#app')
