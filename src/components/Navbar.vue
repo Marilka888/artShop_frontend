15e20e9ad8e050c0c883d0b0441757a219538f6c
Marilka888 <marilka888@mail.ru>
2023-04-28 01:12:43 +0300

    JEW-010 create header

---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="navbar-wrap">
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Art Company</a>
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="#" @click.prevent="signout">Выход</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Navbar',
+  data() {
+    return {
+    };
+  },
+  methods: {
+    signout() {
+      const api = `${process.env.VUE_APP_APIPATH}/logout`;
+      const vm = this;
+      vm.$http.post(api).then((response) => {
+        if (response.data.success) {
+          vm.$router.push('/login');
+          // eslint-disable-next-line
+          alert(response.data.message);
+        }
+      });
+    },
+  },
+};
+</script>
